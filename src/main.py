7c7de81cd970b0ffbcc2f3c5af575426d1e78774
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2022-06-14 23:00:19 +0200

    created main window

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("main")
+#!/usr/bin/env python
+import tkinter as tk
+
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.attributes("-fullscreen", 1)
+        self.configure(background="black")
+
+
+if __name__ == "__main__":
+    main_window = MainWindow()
+    main_window.mainloop()
