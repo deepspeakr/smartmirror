573eee5b505ef79feb3045a97d5dd0e854f92711
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2022-06-20 02:09:29 +0200

    updated config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,8 @@
-weather_api = "hidden"
-ipstack_api = "hidden"
-
+# set True to print debug info
+DEBUG = False
+# API key for openweather
+WEATHER_API = None
+# API key for ipstack
+IPSTACK_API = None
+# URL of RSS to display as the news
+NEWS_URL = "https://news.google.com/rss?topic=h&hl=pl&gl=PL&ceid=PL:pl"
